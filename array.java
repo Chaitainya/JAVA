76054dff82ebc347aa373e657fc3b76859f62088
Chaitainya <chaitainyaa@gmail.com>
2023-03-01 18:20:45 +0530

    new changes

---
+++ array.java
@@ -0,0 +1,13 @@
+public class array {
+    public static void main(String args[]) {
+        int new_arr[] = new int[4];
+        new_arr[0] = 4;
+        new_arr[1] = 6;
+        new_arr[2] = 9;
+        new_arr[3] = 11;
+
+        for (int i = 0; i < new_arr.length; i++) {
+            System.out.println(new_arr[i]);
+        }
+    }
+}
---
+++ scanner.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class scanner {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int new_arr[] = new int[4];
+        new_arr[0] = 4;
+        new_arr[1] = 6;
+        new_arr[2] = 9;
+        new_arr[3] = 11;
+
+        for (int i = 0; i < new_arr.length; i++) {
+            System.out.println(new_arr[i]);
+        }
+    }
+}
